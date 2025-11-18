18487a4e0369403534a4d55b6864227fe30dd7e9
Dariusz <dariusz_bereszynski@op.pl>
2025-11-18 14:36:38 +0100

    Create Book, BookService, BookRepository

---
+++ src/main/java/org/example/service/BookService.java
@@ -0,0 +1,13 @@
+package org.example.service;
+
+import java.util.List;
+import org.example.model.Book;
+
+public interface BookService {
+
+    Book save(Book book);
+
+    List<Book> findAll();
+
+}
+
