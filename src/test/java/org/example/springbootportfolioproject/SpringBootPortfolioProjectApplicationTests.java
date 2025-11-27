cc4a077590cc73fa0a3001c639d3f68b358a2884
Dariusz <dariusz_bereszynski@op.pl>
2025-11-27 13:57:51 +0100

    Create Book, BookService, BookRepository.

---
+++ src/test/java/org/example/springbootportfolioproject/SpringBootPortfolioProjectApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.springbootportfolioproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPortfolioProjectApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
