045a3ec822525da80acae649db6fe06147acef49
Dariusz <dariusz_bereszynski@op.pl>
2025-11-28 15:21:14 +0100

    Remove unnecessary constraints in db file, add adequate response statuses, add omitted signs.

--- src/main/java/org/example/repository/BookRepository.java
+++ src/main/java/org/example/repository/BookRepository.java
@@ -3,5 +3,5 @@ package org.example.repository;
 import org.example.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book,Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
