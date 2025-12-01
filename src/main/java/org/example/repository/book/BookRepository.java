4d5e9e44886d1dcaa9cf8483f8ca99dcec795318
Dariusz <dariusz_bereszynski@op.pl>
2025-12-01 16:37:34 +0100

    Remove implementation of PagingAndSortingRepositor<Book, Long> and change List<BookDto> to Page<BookDto>.

--- src/main/java/org/example/repository/book/BookRepository.java
+++ src/main/java/org/example/repository/book/BookRepository.java
@@ -3,11 +3,10 @@ package org.example.repository.book;
 import org.example.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends PagingAndSortingRepository
-        <Book, Long>, JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+public interface BookRepository extends JpaRepository<Book, Long>,
+        JpaSpecificationExecutor<Book> {
 
 }
