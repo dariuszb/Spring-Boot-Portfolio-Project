b6272f2292d3b47f30058082c9070e4bae1bb5e5
Dariusz <dariusz_bereszynski@op.pl>
2026-02-03 02:43:19 +0100

    Add tests for Book and Category entities.

---
+++ src/test/resources/database/insert-new-books.sql
@@ -0,0 +1,3 @@
+INSERT INTO books(id, title, author, isbn, price) VALUES(1, 'First', 'FirstAuthor', '123456701', 100);
+INSERT INTO books(id, title, author, isbn, price) VALUES(2, 'Second', 'SecondAuthor', '123456702', 200);
+INSERT INTO books(id, title, author, isbn, price) VALUES(3, 'Third', 'ThirdAuthor', '123456703', 300);
