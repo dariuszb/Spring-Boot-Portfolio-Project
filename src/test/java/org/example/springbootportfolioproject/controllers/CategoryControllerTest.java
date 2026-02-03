aa6ff23ff7544a058463c50fdf2f878a8ce0f419
Dariusz <dariusz_bereszynski@op.pl>
2026-02-03 22:31:54 +0100

    CI test.

---
+++ src/test/java/org/example/springbootportfolioproject/controllers/CategoryControllerTest.java
@@ -0,0 +1,203 @@
+package org.example.springbootportfolioproject.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.example.dto.categorydto.CategoryDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
+class CategoryControllerTest {
+
+    protected static MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    public static void beforeAll(
+
+            @Autowired WebApplicationContext applicationContext) throws SQLException {
+
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Sql(scripts = "classpath:database/delete-added-category.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("Varify correctness of creation"
+            + " new instance of category entity")
+    void createCategory_createInputParams_ok() throws Exception {
+
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setName("Category 1");
+        categoryDto.setDescription("Category 1");
+
+        String jsonRequest = objectMapper.writeValueAsString(categoryDto);
+
+        MvcResult result = mockMvc.perform(
+                        post("/api/categories")
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        CategoryDto actual = objectMapper.readValue(result
+                .getResponse()
+                .getContentAsString(), CategoryDto.class);
+
+        EqualsBuilder.reflectionEquals(categoryDto, actual);
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    @DisplayName("Check, that method return all inputs from DB")
+    void getAll_returnAllCategoriesInDB() throws Exception {
+
+        List<CategoryDto> categoryDtoList = getCategoryDtos();
+
+        MvcResult result = mockMvc.perform(
+                        get("/api/categories")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String httpResponse = result.getResponse().getContentAsString();
+
+        JsonNode node = objectMapper.readTree(httpResponse);
+
+        JsonNode content = node.get("content");
+
+        List<CategoryDto> actual = objectMapper.convertValue(
+                content, new TypeReference<List<CategoryDto>>() {
+                });
+
+        List<CategoryDto> sorted = actual.stream()
+                .sorted(Comparator.comparing(CategoryDto::getId)).toList();
+        Assertions.assertEquals(categoryDtoList.size(), actual.size());
+        for (int i = 0; i < actual.size(); i++) {
+            assertThat(categoryDtoList.get(i)).usingRecursiveComparison()
+                    .ignoringFields("id")
+                    .isEqualTo(sorted.get(i));
+        }
+
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = {"USER"})
+    @DisplayName("Varify the correctness of getting with chosen id")
+    void getCategoryById() throws Exception {
+
+        Long chosenCategoryId = 2L;
+
+        CategoryDto categoryDto2 = new CategoryDto();
+        categoryDto2.setId(2L);
+        categoryDto2.setName("crime story");
+        categoryDto2.setDescription("crime story");
+
+        MvcResult result = mockMvc.perform(
+                        get("/api/categories/{id}", chosenCategoryId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String httpResponse = result.getResponse().getContentAsString();
+
+        CategoryDto categoryDtoResult = objectMapper.readValue(httpResponse,
+                new TypeReference<CategoryDto>() {
+            });
+
+        EqualsBuilder.reflectionEquals(categoryDtoResult, categoryDto2);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Sql(scripts = "classpath:database/back-to-values-before-update.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("Varify correctness of updating category with chosen id")
+    void updateCategory_correctInputParams_ok() throws Exception {
+
+        Long categoryIdToUpdate = 2L;
+
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setName("Updated category name");
+        categoryDto.setDescription("Updated category description");
+
+        String jsonRequest = objectMapper.writeValueAsString(categoryDto);
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.put("/api/categories/{id}", categoryIdToUpdate)
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String httpResponse = result.getResponse().getContentAsString();
+
+        CategoryDto actual = objectMapper.readValue(httpResponse,
+            new TypeReference<CategoryDto>() {
+            });
+
+        EqualsBuilder.reflectionEquals(categoryDto, actual);
+
+    }
+
+    private static List<CategoryDto> getCategoryDtos() {
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setId(1L);
+        categoryDto.setName("novel");
+        categoryDto.setDescription("novel");
+
+        CategoryDto categoryDto2 = new CategoryDto();
+        categoryDto2.setId(2L);
+        categoryDto2.setName("crime story");
+        categoryDto2.setDescription("crime story");
+
+        CategoryDto categoryDto3 = new CategoryDto();
+        categoryDto3.setId(3L);
+        categoryDto3.setName("horror");
+        categoryDto3.setDescription("horror");
+
+        List<CategoryDto> categoryDtoList = new ArrayList<>();
+        categoryDtoList.add(categoryDto);
+        categoryDtoList.add(categoryDto2);
+        categoryDtoList.add(categoryDto3);
+        return categoryDtoList;
+    }
+}
---
+++ src/test/java/org/example/springbootportfolioproject/service/categoryservice/CategoryServiceImplTest.java
@@ -0,0 +1,253 @@
+package org.example.springbootportfolioproject.service.categoryservice;
+
+import org.assertj.core.api.Assertions;
+import org.example.dto.bookdto.BookDtoWithoutCategoryIds;
+import org.example.dto.categorydto.CategoryDto;
+import org.example.exceptions.EntityNotFoundException;
+import org.example.mappers.BookMapper;
+import org.example.mappers.CategoryMapper;
+import org.example.model.Book;
+import org.example.model.Category;
+import org.example.repository.book.BookRepository;
+import org.example.repository.category.CategoryRepository;
+import org.example.service.categoryservice.CategoryServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceImplTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private CategoryMapper categoryMapper;
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private BookMapper bookMapper;
+
+    @InjectMocks
+    private CategoryServiceImpl categoryServiceImpl;
+
+    @Test
+
+    void getById_existId_Success() {
+
+        Category category = new Category();
+        category.setId(1L);
+        category.setName("test");
+        category.setDescription("test");
+
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setId(1L);
+        categoryDto.setName("test");
+        categoryDto.setDescription("test");
+
+        Mockito.when(categoryRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+        Mockito.when(categoryMapper.toDto(category)).thenReturn(categoryDto);
+
+        CategoryDto resultCategory = categoryServiceImpl.getById(1L);
+        Assertions.assertThat(categoryDto).isEqualTo(resultCategory);
+    }
+
+    @Test
+    @DisplayName("Verify correctness of updating category with chosen id")
+    void update_correctInputParams_returnUpdatedObject() {
+
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setName("nameToUpdate");
+        categoryDto.setDescription("descriptionToUpdate");
+
+        Category category = new Category();
+        category.setId(1L);
+        category.setName("category1");
+        category.setDescription("description1");
+
+        Long categoryId = 2L;
+
+        Category category2 = new Category();
+        category2.setId(categoryId);
+        category2.setName("category2");
+        category2.setDescription("description2");
+
+        Mockito.when(categoryRepository.existsById(categoryId)).thenReturn(true);
+
+        Mockito.when(categoryRepository.existsById(categoryId))
+                .thenReturn(true);
+
+        Mockito.when(categoryRepository.findById(categoryId))
+                .thenReturn(Optional.of(category));
+
+        Mockito.when(categoryRepository.save(category))
+                .thenReturn(category2);
+
+        Mockito.when(categoryMapper.toDto(category2))
+                .thenReturn(categoryDto);
+
+        CategoryDto updatedCategory = categoryServiceImpl.update(2L, categoryDto);
+
+        Assertions.assertThat(categoryDto).isEqualTo(updatedCategory);
+    }
+
+    @Test
+    @DisplayName("Varify the correct books "
+            + "with chosen category id was returned")
+    void getBookWithoutCategories_correctInputArguments_returnListWithBooks() {
+
+        Category category1 = new Category();
+        category1.setId(1L);
+        category1.setName("category1");
+        category1.setDescription("description2");
+
+        Category category2 = new Category();
+        category2.setId(2L);
+        category2.setName("category2");
+        category2.setDescription("description2");
+
+        Book book1 = new Book();
+        book1.setId(1L)
+                .setTitle("Title1")
+                .setAuthor("Author1")
+                .setIsbn("123456789")
+                .setPrice(BigDecimal.valueOf(100))
+                .setCategories(Set.of(category1))
+                .setDescription("Description1")
+                .setCoverImage("CoverImage1");
+
+        BookDtoWithoutCategoryIds bookDto1 = new BookDtoWithoutCategoryIds();
+        bookDto1.setId(1L)
+                .setTitle("Title1")
+                .setAuthor("Author1")
+                .setIsbn("123456789")
+                .setPrice(BigDecimal.valueOf(100))
+                .setDescription("Description1")
+                .setCoverImage("CoverImage1");
+
+        Book book2 = new Book();
+        book2.setId(2L)
+                .setTitle("Title2")
+                .setAuthor("Author2")
+                .setIsbn("123456788")
+                .setPrice(BigDecimal.valueOf(100))
+                .setCategories(Set.of(category2))
+                .setDescription("Description2")
+                .setCoverImage("CoverImage2");
+
+        BookDtoWithoutCategoryIds bookDto2 = new BookDtoWithoutCategoryIds();
+        bookDto2.setId(2L)
+                .setTitle("Title2")
+                .setAuthor("Author2")
+                .setIsbn("123456788")
+                .setPrice(BigDecimal.valueOf(100))
+                .setDescription("Description2")
+                .setCoverImage("CoverImage2");
+
+        Mockito.when(categoryRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(categoryRepository.existsById(2L)).thenReturn(true);
+        Mockito.when(bookRepository.findAllByCategoryId(1L)).thenReturn(
+                List.of(book1));
+        Mockito.when(bookMapper.toDtoWithoutCategories(book1)).thenReturn(
+                bookDto1);
+
+        Mockito.when(bookRepository.findAllByCategoryId(2L)).thenReturn(
+                List.of(book2));
+        Mockito.when(bookMapper.toDtoWithoutCategories(book2)).thenReturn(
+                bookDto2);
+
+        List<BookDtoWithoutCategoryIds> bookWithoutCategories1 =
+                categoryServiceImpl.getBookWithoutCategories(1L);
+
+        Assertions.assertThat(bookWithoutCategories1).isEqualTo(List.of(bookDto1));
+
+        List<BookDtoWithoutCategoryIds> bookWithoutCategories2 =
+                categoryServiceImpl.getBookWithoutCategories(2L);
+
+        Assertions.assertThat(bookWithoutCategories2).isEqualTo(List.of(bookDto2));
+
+    }
+
+    @Test
+    @DisplayName("Check, that exception is throwing "
+            + "when get books with non exist category id")
+    void getBookWithoutCategories_nonExistingId_thrownException() {
+
+        Mockito.when(categoryRepository.existsById(Mockito.anyLong()))
+                .thenReturn(false);
+
+        Exception exception = assertThrows(EntityNotFoundException.class,
+                () -> categoryServiceImpl.getById(2L));
+
+        String expectedMessage = "Category with id 2 doesn't exist";
+
+        Assertions.assertThat(expectedMessage).isEqualTo(exception.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("Varify the books are returned "
+            + "when the DB is not empty")
+    void findAll_nonEmptyDB_returnAllInputsFromDB() {
+        Category category1 = new Category();
+        category1.setId(1L);
+        category1.setName("category1");
+        category1.setDescription("description1");
+
+        Category category2 = new Category();
+        category2.setId(2L);
+        category2.setName("category2");
+        category2.setDescription("description2");
+
+        CategoryDto categoryDto1 = new CategoryDto();
+        categoryDto1.setId(1L);
+        categoryDto1.setName("categoryDto1");
+        categoryDto1.setDescription("descriptionDto1");
+
+        CategoryDto categoryDto2 = new CategoryDto();
+        categoryDto2.setId(2L);
+        categoryDto2.setName("categoryDto2");
+        categoryDto2.setDescription("descriptionDto2");
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(category1);
+        categoryList.add(category2);
+
+        Mockito.when(categoryRepository.findAll(Pageable.unpaged()))
+                .thenReturn(new PageImpl<Category>(categoryList));
+
+        Mockito.when(categoryMapper.toDto(category1))
+                .thenReturn(categoryDto1);
+        Mockito.when(categoryMapper.toDto(category2))
+                .thenReturn(categoryDto2);
+
+        Page<CategoryDto> all = categoryServiceImpl.findAll(Pageable.unpaged());
+
+        List<CategoryDto> allList = all.stream().toList();
+        Assertions.assertThat(all.getTotalElements()).isEqualTo(2);
+
+        Assertions.assertThat(categoryMapper.toDto(category1))
+                .isEqualTo(allList.get(0));
+        Assertions.assertThat(categoryMapper.toDto(category2))
+                .isEqualTo(allList.get(1));
+
+    }
+}
