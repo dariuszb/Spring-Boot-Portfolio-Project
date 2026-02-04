3f02af216a1fdb345ed6f1b8b22d33126b27ca2c
Dariusz <dariusz_bereszynski@op.pl>
2026-02-04 20:19:30 +0100

    Add test for Book and Category entities.

--- src/test/java/org/example/springbootportfolioproject/SpringBootPortfolioProjectApplicationTests.java
+++ src/test/java/org/example/springbootportfolioproject/SpringBootPortfolioProjectApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 class SpringBootPortfolioProjectApplicationTests {
+
     @Test
     void contextLoads() {
     }
