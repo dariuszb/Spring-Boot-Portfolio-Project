8223cca70417c16c81d0d23949ac0449d8682edc
Dariusz <dariusz_bereszynski@op.pl>
2026-02-05 23:01:33 +0100

    Add tests for Book and Category entities.

---
+++ src/test/resources/database/insert-categories-to-categories-table.sql
@@ -0,0 +1,3 @@
+INSERT INTO categories(name, description, is_deleted) VALUES('novel', 'novel', 0);
+INSERT INTO categories(name, description, is_deleted) VALUES('crime story', 'crime story', 0);
+INSERT INTO categories(name, description, is_deleted) VALUES('horror', 'horror', 0);
---
+++ src/test/resources/database/insert-new-books.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories(id, name, description, is_deleted) VALUES(1, 'crime story', 'crime story', 0);
+
+INSERT INTO books(id, title, author, isbn, price, is_deleted) VALUES(1,'First', 'FirstAuthor', '123456701', 100,  0);
+INSERT INTO books(id, title, author, isbn, price, is_deleted) VALUES(2,'Second', 'SecondAuthor', '123456702', 200, 0);
+INSERT INTO books(id, title, author, isbn, price, is_deleted) VALUES(3,'Third', 'ThirdAuthor', '123456703', 300, 0 );
