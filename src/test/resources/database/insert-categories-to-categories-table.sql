d57aa16b6fcdb5c8a1574664ae5132468c8cee32
Dariusz <dariusz_bereszynski@op.pl>
2026-02-05 23:56:20 +0100

    Add tests for Book and Category entities.

---
+++ src/test/resources/database/insert-categories-to-categories-table.sql
@@ -0,0 +1,3 @@
+INSERT INTO categories(id, name, description, is_deleted) VALUES(1, 'novel', 'novel', 0);
+INSERT INTO categories(id, name, description, is_deleted) VALUES(2, 'crime story', 'crime story', 0);
+INSERT INTO categories(id, name, description, is_deleted) VALUES(3, 'horror', 'horror', 0);
